10aec279589edab5156df4591dd31c838f97569a
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-17 17:27:30 +0500

    Add update and delete product controllers

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -19,3 +19,47 @@ export const getAllProducts = async (req, res, next) => {
     products,
   });
 };
+
+
+// Update Product - Admin
+export const updateProduct = async (req, res, next) => {
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+
+
+// Delete Product - Admin
+export const deleteProduct = async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+};
--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getAllProducts,createProduct } from "../controllers/productController.js";
+import { getAllProducts,createProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
-// Get all products
+
 router.get("/product", getAllProducts);
-router.post("/product/new",createProduct);
+router.post("/product/new", createProduct);
+router.put("/product/:id", updateProduct);
+router.delete("/product/:id",deleteProduct);
+
+
 
 export default router;
