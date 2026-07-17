bc20886c7e5a35500b0cb355270cf7dbb76379ab
Areeba Waqar <areebawaqar007@gmail.com>
2026-07-17 20:46:38 +0500

    Handle cast errors and unhandled promise rejections

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
-
 import app from "./app.js";
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
 // Load env variables
 dotenv.config({
   path: "./server/.env",
 });
+
+let server;
 
 const startServer = async () => {
   try {
@@ -18,13 +27,22 @@ const startServer = async () => {
 
     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => {
+    server = app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-
   } catch (error) {
     console.log("Server failed to start:", error);
   }
 };
 
 startServer();
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise Rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
